7ff2dba69c7131676966da29f930a204c8dc01ba
syiism <syiism@163.com>
2026-07-19 07:29:09 +0800

    refactor: luomu search uses tab_type with number-to-Chinese mapping from fq_moduleMap

--- handlers/luomu/search.py
+++ handlers/luomu/search.py
@@ -8,20 +8,30 @@ from base.base import HandlerRegistry
 from base.searchBase import SearchBaseHandler, SearchResponse
 from utils.fq_utils import DEFAULT_TIMEOUT, build_book_item, extract_book_data, normalize_api_base, strip_search_prefix
 
+TAB_MAP: dict[str, str] = {
+    "3": "小说",
+    "2": "听书",
+    "8": "漫画",
+    "11": "短剧",
+    "19": "漫剧",
+    "13": "短篇",
+    "12": "出版",
+}
+
 
 @HandlerRegistry.register
 class LuomuSearchHandler(SearchBaseHandler):
     path = "/luomu/search"
     name = "luomu_search"
     methods = ["GET"]
-    query_params = ["base_url", "query", "page", "tab"]
-    description = "luomu 搜索"
+    query_params = ["base_url", "query", "page", "tab_type"]
+    description = "luomu 搜索（tab_type 数字映射中文 tab）"
 
     async def handle(self, **kwargs: Any) -> SearchResponse:
         base_url = normalize_api_base(kwargs.get("base_url", ""), "/api")
         query = strip_search_prefix(kwargs.get("query", ""))
         page = kwargs.get("page", "1")
-        tab = kwargs.get("tab", "小说")
+        tab = TAB_MAP.get(kwargs.get("tab_type", ""), "小说")
 
         url = f"{base_url}/search?source=番茄&page={page}&query={query}&tab={tab}"
 
